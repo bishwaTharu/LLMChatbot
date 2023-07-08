4cf2bc1b32a3c646b0e4fc35676e211323d5cc3a
bishwaTharu <bishwanaththaru34@gmail.com>
2023-07-08 11:47:28 +0545

    ui changed

--- webui.py
+++ webui.py
@@ -4,18 +4,30 @@ from client.chat import prompt
 from utils.tts import get_speech_input
 from utils.stt import Speaker
 
+# CSS styling for the title
+st.markdown(
+    """
+    <style>
+    .title {
+        text-align: center;
+        font-size: 36px;
+        margin-bottom: 30px;
+        color: #325d88;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-st.title('LLM Chatbot')
-if st.button("Chat"):
+st.markdown('<p class="title">LLM Chatbot</p>', unsafe_allow_html=True)
+
+if st.button("Ask Me Anything",type='primary'):
     my_text = get_speech_input()
     if my_text:
         st.text_input('Prompt', f'{my_text}')
         chatbot_response = prompt(my_text)
-        # st.text_input('Chatbot Response', chatbot_response)
         st.text_area('Chatbot Response', chatbot_response)
-        # st.write(title)
         Speaker(chatbot_response)
-
     else:
-        title = st.text_input('Chatbot Response', "Please First make a prompt")
-
+        st.text_input('Chatbot Response', "Please first make a prompt")
