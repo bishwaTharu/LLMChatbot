26f23aab9eed6b1644730a5566e324d759ca314b
bishwaTharu <bishwanaththaru34@gmail.com>
2023-07-12 08:34:30 +0545

    integration llms with Ray

--- webui.py
+++ webui.py
@@ -4,22 +4,22 @@ from client.chat import prompt
 from utils.tts import Hear
 from utils.stt import Speaker
 
-st.markdown(
-    """
-    <style>
-    .title {
-        text-align: center;
-        font-size: 36px;
-        margin-bottom: 30px;
-        color: #325d88;
-        font-weight: bold;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.markdown(
+#     """
+#     <style>
+#     .title {
+#         text-align: center;
+#         font-size: 36px;
+#         margin-bottom: 30px;
+#         color: #325d88;
+#         font-weight: bold;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
-st.markdown('<p class="title">LLM Chatbot</p>', unsafe_allow_html=True)
+# st.markdown('<p class="title">LLM Chatbot</p>', unsafe_allow_html=True)
 
 if st.button("Ask Me Anything",type='primary'):
     my_text = Hear()
