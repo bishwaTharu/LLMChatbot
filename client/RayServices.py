3338b2b79a624c9a1c18c81969e621cb8c5bf898
bishwaTharu <bishwanaththaru34@gmail.com>
2023-07-12 14:23:02 +0545

    Integration with ray

--- client/RayServices.py
+++ client/RayServices.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 SERVER_IP = os.getenv("BASE_URL")
 
-def distributedChat(prompt:str):
+def distributedChat(prompt:str)->str:
     response_ref = requests(f"{SERVER_IP}/?text={prompt}")
     response = response_ref.content.decode()
     return response
