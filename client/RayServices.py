5d6e94828b67296cba78cf6077921df6669887c2
bishwaTharu <bishwanaththaru34@gmail.com>
2023-07-18 17:08:29 +0545

    streaming support in Ray

--- client/RayServices.py
+++ client/RayServices.py
@@ -5,6 +5,6 @@ load_dotenv(find_dotenv())
 SERVER_IP = os.getenv("BASE_URL")
 
 def distributedChat(prompt:str)->str:
-    response_ref = requests(f"{SERVER_IP}/?text={prompt}")
+    response_ref = requests.post(f"{SERVER_IP}/?text={prompt}")
     response = response_ref.content.decode()
     return response
--- webui.py
+++ webui.py
@@ -3,15 +3,20 @@ import streamlit as st
 from client.RayServices import distributedChat
 
 from utils.tts import Hear
-from utils.stt import Speaker
+# import win32com.client
+# speaker = win32com.client.Dispatch("SAPI.SpVoice")
 
+# def Speaker(text):
+#    speaker.Speak(text)
+# from utils.stt import Speaker
 
+st.title("LLM CHATBOT")
 if st.button("Ask Me Anything",type='primary'):
     my_text = Hear()
     if my_text:
         st.text_input('Prompt', f'{my_text}')
         chatbot_response = distributedChat(my_text)
         st.text_area('Chatbot Response', chatbot_response)
-        Speaker(chatbot_response)
+        # Speaker(chatbot_response)
     else:
         st.text_input('Chatbot Response', "Please first make a prompt")
